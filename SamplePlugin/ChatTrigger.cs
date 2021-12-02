3c8e93507e74fd8a6da99541e17d7fe9173a4692
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2021-12-02 18:46:42 -0500

    Begin implementing triggers

---
+++ SamplePlugin/ChatTrigger.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Game.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AetherSenseRedux
+{
+    internal class ChatTrigger : ITrigger
+    {
+        public void Match(XivChatType type, string sender, string message)
+        {
+
+        }
+    }
+}

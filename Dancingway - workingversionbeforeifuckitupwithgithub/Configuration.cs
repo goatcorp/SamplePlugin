d7a7397e06b9f531ae8c15c8e663d7e170fcb0d4
Callistemon Y'amadori <aaron.spain@gmail.com>
2022-12-20 10:40:55 +1100

    ffs how do i push this shit

---
+++ Dancingway - workingversionbeforeifuckitupwithgithub/Configuration.cs
@@ -0,0 +1,28 @@
+using Dalamud.Configuration;
+using Dalamud.Plugin;
+using System;
+
+namespace Dancingway
+{
+    [Serializable]
+    public class Configuration : IPluginConfiguration
+    {
+        public int Version { get; set; } = 0;
+
+        public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+
+        // the below exist just to make saving less cumbersome
+        [NonSerialized]
+        private DalamudPluginInterface? PluginInterface;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.PluginInterface = pluginInterface;
+        }
+
+        public void Save()
+        {
+            this.PluginInterface!.SavePluginConfig(this);
+        }
+    }
+}

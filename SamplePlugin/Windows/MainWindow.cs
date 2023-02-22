6c1f67367437affe22534d66a8cebb94814b5bf4
byteknown <byteknown@gmail.com>
2023-02-22 17:35:57 +0100

    window commit

--- SamplePlugin/Windows/MainWindow.cs
+++ SamplePlugin/Windows/MainWindow.cs
@@ -7,7 +7,7 @@ namespace SamplePlugin.Windows;
 
 public class MainWindow : Window, IDisposable
 {
-    public MainWindow() : base("My Amazing Window", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    public MainWindow() : base("Baccarat by Moonhell", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
         SizeConstraints = new WindowSizeConstraints
         {
@@ -24,12 +24,21 @@ public class MainWindow : Window, IDisposable
     public override void Draw()
     {
         ImGui.Text($"This is your main window. You won't need any more window if you plan on using tabs.");
-
+        string player = "Moonhell";
+        string player2 = "Klyhia";
         if (ImGui.Button("This is a button"))
         {
             // Do something when pressed
         }
-
+        ImGui.Text(player);
+        if (ImGui.Button("This is a button"))
+        {
+            // Do something when pressed
+        }
+        if (ImGui.Button("This is a button"))
+        {
+            // Do something when pressed
+        }
         // There's alot more ways to interact, it's all under ImGui.<something>. Play with it !
     }
 }

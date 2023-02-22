d13a315d669ce752199e462d6ea9ee71764bb021
byteknown <byteknown@gmail.com>
2023-02-22 18:56:23 +0100

    dunno what happens again

--- SamplePlugin/Windows/MainWindow.cs
+++ SamplePlugin/Windows/MainWindow.cs
@@ -1,9 +1,9 @@
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
+using Lumina.Excel.GeneratedSheets;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Numerics;
-
 namespace SamplePlugin.Windows;
 
 public class MainWindow : Window, IDisposable
@@ -23,29 +23,9 @@ public class MainWindow : Window, IDisposable
     {
         GC.SuppressFinalize(this);
     }
-
-    public override void Draw()
+    public void GeneratePlayers(string playerName)
     {
-        ImGui.Text($"This is your main window. You won't need any more window if you plan on using tabs.");
-        string host = "Moonhell";
-        string player1 = "Kestra";
-        string player2 = "Klyhia";
-        
-        int totalBet = player1Bet+ player2Bet;
-        if (ImGui.Button("This is a button"))
-        {
-            // Do something when pressed: enable disable plugin
-        }
-        ImGui.Text(host);
-        ImGui.SameLine();
-        ImGui.Text("total bet");
-        ImGui.SameLine();
-        if (ImGui.Button("This is a button"))
-        {
-            // Do something when pressed
-        }
-        ImGui.NewLine();
-        ImGui.Text(player1);
+        ImGui.Text(playerName);
         ImGui.SameLine();
         ImGui.InputInt("some value", ref player1Bet, 1, 2, 0);
         ImGui.SameLine();
@@ -53,15 +33,36 @@ public class MainWindow : Window, IDisposable
         {
             // Do something when pressed -> Hit a card
         }
-        ImGui.NewLine();
-        ImGui.Text(player2);
-        ImGui.SameLine();
-        ImGui.InputInt("some value", ref player2Bet, 1, 2, 0);
-        ImGui.SameLine();
+    }
+    public override void Draw()
+    {
+        string host = "Moonhell";
+        int totalBet = player1Bet + player2Bet;
+        ImGui.Text($"This is your main window. You won't need any more window if you plan on using tabs.");
         if (ImGui.Button("This is a button"))
         {
-            // Do something when pressed -> Hit a card
+            // Do something when pressed: enable disable plugin
+        }
+        ImGui.BeginTable("Table1", 2);
+    ImGui.TableSetupColumn("Player");
+    ImGui.TableSetupColumn("Bets");
+    ImGui.TableSetupColumn("Cards");
+    ImGui.TableNextRow();
+        ImGui.Text(host);
+        ImGui.TableNextColumn();
+        ImGui.Text("total bet");
+        ImGui.TableNextColumn();
+        if (ImGui.Button("This is a button"))
+        {
+            // Do something when pressed
+        }
+        ImGui.TableNextRow();
+        for (int i = 1; i <= 8; i++)
+        {
+            GeneratePlayers("Player " + i);
+            ImGui.TableNextRow();
         }
 
+        ImGui.EndTable();
     }
 }

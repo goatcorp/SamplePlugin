82cca9a4068095898a4b3ced52e231c512031544
Infi <infiziert@protonmail.ch>
2025-12-18 21:33:51 +0100

    - API 14 - Use PlayerState for most things - Switch TextUnformatted to Text

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -14,6 +14,7 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
     [PluginService] internal static IClientState ClientState { get; private set; } = null!;
+    [PluginService] internal static IPlayerState PlayerState { get; private set; } = null!;
     [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
 
